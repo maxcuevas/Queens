aadedfdae067d489afdf95a5099c220bcc45ae3f
Max Cuevas <maximilianocuevas777@gmail.com>
2018-11-03 14:56:41 -0700

    Solution for the problem, not pretty but it is recursive!

--- src/Board.java
+++ src/Board.java
@@ -4,61 +4,69 @@ import java.util.Optional;
 
 class Board {
 
-  private Optional<Board> board = Optional.empty();
-  private Queen queen;
+    private Optional<Board> board = Optional.empty();
+    private Queen queen;
+    private int boardStart = 0;
+    private int boardEnd = 7;
+    private int boardSize = 8;
 
-  Board() {
-  }
-
-  private Board(Board board, Queen queen) {
-    this.board = Optional.of(board);
-    this.queen = queen;
-  }
-
-  Board placeQueenAt(int x, int y) {
-    if (x < 0 || 7 < x) {
-      return this;
+    Board() {
     }
 
-    if (y < 0 || 7 < y) {
-      return this;
+    private Board(Board board, Queen queen) {
+        this.board = Optional.of(board);
+        this.queen = queen;
     }
 
-    return new Board(this, new Queen(x, y));
-  }
-
-  private List<Queen> getAllQueens() {
-    List<Queen> acc = new LinkedList<>();
-    Board next = this;
-    while (next.board.isPresent()) {
-      acc.add(next.queen);
-      next = next.board.get();
+    public int getBoardSize() {
+        return boardSize;
     }
-    return acc;
-  }
 
-  @Override
-  public String toString() {
 
-    StringBuilder result = new StringBuilder();
+    Board placeQueenAt(int x, int y) {
+        if (x < boardStart || boardEnd < x) {
+            return this;
+        }
 
-    for (int y = 0; y < 8; y++) {
-      for (int x = 0; x < 8; x++) {
-        result.append(isOccupied(x, y) ? "[Q]" : "[ ]");
-      }
-      result.append("\n");
+        if (y < boardStart || boardEnd < y) {
+            return this;
+        }
+
+        return new Board(this, new Queen(x, y));
     }
-    return result.toString();
-  }
 
-  private boolean isOccupied(int x, int y) {
-    return getAllQueens().stream()
-        .anyMatch(queen -> queen.getX() == x && queen.getY() == y);
-  }
+    public List<Queen> getAllQueens() {
+        List<Queen> acc = new LinkedList<>();
+        Board next = this;
+        while (next.board.isPresent()) {
+            acc.add(next.queen);
+            next = next.board.get();
+        }
+        return acc;
+    }
 
-  boolean isValid() {
-    List<Queen> allQueens = getAllQueens();
-    return allQueens.stream().noneMatch(queen0 -> allQueens.stream().anyMatch(queen0::canAttack));
-  }
+    @Override
+    public String toString() {
+
+        StringBuilder result = new StringBuilder();
+
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
+                result.append(isOccupied(x, y) ? "[Q]" : "[ ]");
+            }
+            result.append("\n");
+        }
+        return result.toString();
+    }
+
+    private boolean isOccupied(int x, int y) {
+        return getAllQueens().stream()
+                .anyMatch(queen -> queen.getX() == x && queen.getY() == y);
+    }
+
+    boolean isValid() {
+        List<Queen> allQueens = getAllQueens();
+        return allQueens.stream().noneMatch(queen0 -> allQueens.stream().anyMatch(queen0::canAttack));
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,98 @@
 import java.util.LinkedList;
+import java.util.List;
+import java.util.OptionalInt;
+
+import static java.lang.System.exit;
 
 public class Main {
 
-  public static void main(String[] args) {
-    LinkedList<Board> boards = new LinkedList<>();
-    boards.add(new Board());
+    public static void main(String[] args) {
+//      LinkedList<Board> boards = new LinkedList<>();
+//      boards.add(new Board());
+//    for (int queen = 0; queen < 8; queen++) {
+//      for (int y = 0; y < 8; y++) {
+//        for (int x = 0; x < 8; x++) {
+//          Board next = boards.getLast().placeQueenAt(x, y);
+//          if (next.isValid()) {
+//            boards.add(next);
+//          }
+//        }
+//      }
+//    }
 
-    for (int queen = 0; queen < 8; queen++) {
-      for (int y = 0; y < 8; y++) {
-        for (int x = 0; x < 8; x++) {
-          Board next = boards.getLast().placeQueenAt(x, y);
-          if (next.isValid()) {
-            boards.add(next);
-          }
+
+//    System.out.println(  boards.getLast());
+        try {
+            System.out.println(findViableBoard());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-      }
+
     }
 
+    private static Board findViableBoard() throws InterruptedException {
 
-    System.out.println(boards.getLast());
-  }
+        LinkedList<Board> boards = new LinkedList<>();
+        boards.add(new Board());
+
+        int newX = 0;
+        int newY = 0;
+
+
+        boards.getLast().placeQueenAt(newX, newY);
+
+        return recursiveBoards(boards, newX, newY);
+    }
+
+    private static Board recursiveBoards(LinkedList<Board> boards, int newX, int newY) throws InterruptedException {
+
+        //placeQueenAt stops me from ever putting something in 8
+
+        Board currentBoard = boards.getLast().placeQueenAt(newX, newY);
+        boards.add(currentBoard);
+
+//        Thread.sleep(250);
+        System.out.println(currentBoard);
+
+        if (isOutOfBounds(newY, currentBoard)) {
+            List<Queen> allQueens = boards.getLast().getAllQueens();
+            OptionalInt maxX = allQueens.stream().mapToInt(x -> x.getX()).max();
+
+            if (!maxX.isPresent()) {
+                System.out.println("queen did not have exist");
+                exit(0);
+            }
+
+
+            OptionalInt oldY = allQueens.stream()
+                    .filter(queen -> queen.getX() == maxX.getAsInt())
+                    .mapToInt(y -> y.getY())
+                    .findFirst();
+
+            boards.removeLast();
+            boards.removeLast();
+            recursiveBoards(boards, maxX.getAsInt(), oldY.getAsInt() + 1);
+
+        } else if (!currentBoard.isValid()) {
+            boards.removeLast();
+            recursiveBoards(boards, newX, newY + 1);
+        } else if (!isFullBoard(currentBoard)) {
+            newX = currentBoard.getAllQueens().size();
+            recursiveBoards(boards, newX, 0);
+        }
+
+
+        return boards.getLast();
+    }
+
+    private static boolean isFullBoard(Board currentBoard) {
+        return currentBoard.getAllQueens().size() == currentBoard.getBoardSize();
+    }
+
+    private static boolean isOutOfBounds(int newY, Board board) {
+
+        return newY == board.getBoardSize() ? true : false;
+    }
+
 
 }
